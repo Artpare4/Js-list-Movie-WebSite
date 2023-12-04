521bc4b30cf9d49e18a23155ff5600f10731b0e8
Arthur Parent <arthur.parent@etudiant.univ-reims.fr>
2023-12-04 08:45:29 +0100

    Ajout de la fonction getAllMovies(), tests OK

--- src/movies-api.js
+++ src/movies-api.js
@@ -1,1 +1,8 @@
 export const API_URL="http://movies-api";
+
+export function getAllMovies(){
+    let $request;
+    $request=fetch(`${API_URL}/movies`);
+    return $request.then((response)=>response.json())
+}
+
